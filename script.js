8215b23305e40d4a43a94a2886d4558be82a3710
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-29 15:05:47 +0200

    Added array object

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('JS OK')
+
+//*SNACK 1*
+//Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+//Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
+
+const tableName = 'tavolo vip';
+
+const guestes = [
+    { tableName: tableName, guestName: 'Brad Pitt'},
+    { tableName: tableName, guestName: 'Johnny Depp'},
+    { tableName: tableName, guestName: 'Lady Gaga'},
+    { tableName: tableName, guestName: 'Cristiano Ronaldo'},
+    { tableName: tableName, guestName: 'Georgina Rodriguez'},
+    { tableName: tableName, guestName: 'Chiara Ferragni'},
+    { tableName: tableName, guestName: 'George Clooney'},
+    { tableName: tableName, guestName: 'Amal Clooney'},
+    { tableName: tableName, guestName: 'Fedez'},
+    { tableName: tableName, guestName: 'Amadeus'},
+    { tableName: tableName, guestName: 'Fiorello'},
+]
