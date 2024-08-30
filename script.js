32116e0884fb7e478f3bf7036d3867ee12bceb07
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-30 15:10:14 +0200

    Added array object snack 4#

--- script.js
+++ script.js
@@ -118,5 +118,19 @@ const  {bikeName , weight} = lessWeightBike;
 console.log(`La bici più leggere è la ${bikeName} con il peso ${weight}`)
 
 
+// *SNACK 4*
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
+// Creo la mia ARRAY di OBJECTS
+const teams = [
+  {teamName: 'Barcelona' , points: 0, faulTaken: 0},
+  {teamName: 'Real Madrid' , points: 0, faulTaken: 0},
+  {teamName: 'Roma' , points: 0, faulTaken: 0},
+  {teamName: 'Juventus' , points: 0, faulTaken: 0},
+  {teamName: 'Napoli' , points: 0, faulTaken: 0},
+]
 
