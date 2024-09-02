96fc90b11d55702a3e54c03a14460f2b4c1b5f47
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-02 12:32:39 +0200

    Terzo snack 02-09

--- file-js/script2.js
+++ file-js/script2.js
@@ -19,3 +19,24 @@ const filteredAnimals = animals.filter((animal) => {
 })
 
 console.log(filteredAnimals)
+
+// * Crea un array di oggetti che rappresentano delle persone.
+// * Ogni persona ha un nome, un cognome e un'età.
+
+// * Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+const students = [
+    { nome: 'franco', cognome: 'frossi', età: 8 },
+    { nome: 'marco', cognome: 'bianchi', età: 12 },
+    { nome: 'simona', cognome: 'neri', età: 30 },
+    { nome: 'luca', cognome: 'gialli', età: 60 },
+    { nome: 'peppe', cognome: 'verdi', età: 80 },
+    { nome: 'gina', cognome: 'viola', età: 25 },
+]
+
+let studentsWhoCanDrive = students.forEach((student) =>{
+    if(student.età > 17 & student.età < 70) 
+    console.log(`il sottoscritto ${student.nome} ${student.cognome} può guidare`);
+
+})
+
