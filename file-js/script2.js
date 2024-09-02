6877d358d3157c80f98fcde791491e84684f8c9e
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-02 12:02:54 +0200

    Second snack 02-09

--- file-js/script2.js
+++ file-js/script2.js
@@ -1,1 +1,19 @@
 console.log('JS OK')
+
+// *Crea un array di oggetti che rappresentano degli animali.
+// *Ogni animale ha un nome, una famiglia e una classe.
+
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: 'tigre', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'topo', famiglia: 'roditori', classe: 'mammiferi' },
+    { nome: 'falco', famiglia: 'falconidea', classe: 'uccelli' },
+]
+
+const filteredAnimals = animals.filter((animal) => {
+    return animal.classe === 'mammiferi';
+})
+
+console.log(filteredAnimals)
